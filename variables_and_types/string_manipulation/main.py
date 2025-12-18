767c49158a29f135277db20759acd999b17da906
BN-Master <>
2025-12-18 23:51:52 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,10 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+bakery1 = grocery_items[12:17]
+#dairy4 = grocery_items[18:24]
+#dairy5 = grocery_items[25:32]
+#dairy6 = grocery_items[33:]
+
+message = "We have dairy and bakery items: " + dairy1 + ", " + dairy2 + ", and " + bakery1 + " in aisle 5"
+print(message)
